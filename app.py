e13051d85ea59a1cf0ed14b936c0df9b2ec3ba87
Leew1234 <wyattlee5252008@gmail.com>
2024-02-25 19:55:18 -0500

    pushing to git hub

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template , request , redirect 
+from dotenv import load_dotenv
+import pymongo
+import random
+import os
+app=Flask("Jumbled_Words")
+app_path=os.path.join(os.path.dirname(__file__),".")
+dotenv_path=os.path.join(app_path,".env")
+load_dotenv(dotenv_path)
+connectionString = os.environ.get("MONGODB_URI")
+cluster = pymongo.MongoClient(connectionString)
+database = cluster["mydatabase"]
+collection = database["words"]
+
+
+@app.route("/", methods= ["GET","POST"])
+def index():
+    if request.method=="POST":
+        word =request.form["words"]
+        word=word.lower()
+        wordlist=list(word)
+        random.shuffle(wordlist)
+        jumbledword="".join(wordlist)
+        collection.insert_one({"orginal-word":word,"jumbled-word":jumbledword})
+        return redirect("/")
+    else:
+        return render_template("index.html")
+
+
+
+@app.route("/play", methods=["GET","POST"])
+def play():
+    if request.method=="GET":
+        words=list(collection.find())
+        return render_template("play.html",words=words)
+    else:
+        count=0
+        original_words=request.form.getlist("original-word")
+        inputed=request.form.getlist("user-input")
+        print(inputed,original_words)
+        for n in range (0,len(inputed),1):
+            if original_words[n]==inputed[n]:
+                count=count+1
+        return render_template("result.html",count=count,original_words=original_words,inputed=inputed)
+
+
+app.run(debug=True)
